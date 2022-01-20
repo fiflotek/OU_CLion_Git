c63ab1f974ec0db6e926a1964c3bb66aa848d0cb
fiflotek <filippunko@gmail.com>
2022-01-20 23:43:38 +0100

    zmiana na mnożenie w metodzie policz

--- main.cpp
+++ main.cpp
@@ -2,11 +2,11 @@
 
 int policz(int a,int b)
 {
-    return a+b;
+    return a*b;
 }
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
-    std::cout << "2+3=" << policz(2,3)<<std::endl;
+    std::cout << "2*3=" << policz(2,3)<<std::endl;
     return 0;
 }
